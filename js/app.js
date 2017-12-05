92b3f1849d2eb85bbc86de8606d367f3eca97588
bryaneaton <sfasu77@gmail.com>
2017-12-05 13:09:08 -0500

    Computed properties and watch properties

--- js/app.js
+++ js/app.js
@@ -4,10 +4,20 @@ new Vue({
         counter: 0,
         secondCounter: 0
     },
+    // Computed properties is best practice
     computed: {
         output: function () {
             console.log('Computed');
             return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
+        }
+    },
+    // not best practice
+    watch: {
+        counter: function (value) {
+            var vm = this;
+            setTimeout(function () {
+                vm.counter = 0;
+            }, 2000);
         }
     },
     methods: {

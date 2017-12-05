23c79240ed7d303be98caf5e172532700ccdf88b
bryaneaton <sfasu77@gmail.com>
2017-12-05 13:22:17 -0500

    Exercise #3

--- js/app.js
+++ js/app.js
@@ -1,29 +1,24 @@
 new Vue({
-    el: '#app',
+    el: '#exercise',
     data: {
-        counter: 0,
-        secondCounter: 0
+        counter: 0
     },
-    // Computed properties is best practice
     computed: {
-        output: function () {
-            console.log('Computed');
-            return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
+        result: function () {
+            return this.counter < 37 ? 'not there yet' : 'done'
         }
     },
-    // not best practice
     watch: {
         counter: function (value) {
             var vm = this;
             setTimeout(function () {
                 vm.counter = 0;
-            }, 2000);
-        }
-    },
-    methods: {
-        result: function () {
-            console.log('Method');
-            return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
+            }, 5000);
         }
     }
 });
+
+
+//Show a "result" of 'not there yet' as long as "value" is not equal to 37 - you can change "value" with the buttons. Print 'done' once you did it
+
+//TODO: Watch for changes in the "result" and reset the "value" after 5 seconds (hint: setTimeout(..., 5000)
